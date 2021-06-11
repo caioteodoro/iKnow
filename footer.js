f72aa19bc6a3976065c2e9a1f7ae1d852f189a58
caioteodoro <caiorodrigot@hotmail.com>
2021-06-10 23:34:59 -0300

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,14 @@
+var name = "Caio Teodoro";
+var year = 2022;
+var twitter = '@_caioteodoro';
+var instagram = '@_caioteodoro';
+var sexo = 'Masculino';
+var polo = 'Higienópolis';
+
+
+document.write(name +" ");
+document.write(year +" ");
+document.write('Twitter: '+ twitter + " ");
+document.write('Instagram: '+ instagram + " ");
+document.write('Sexo: '+ sexo + " ");
+document.write('Polo: '+ polo + " ");
